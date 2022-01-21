566db25c9d8e666604f025f34d1e89d6aa88dbb3
RajendranEshwaran <rajendranmca2010@gmail.com>
2022-01-21 11:08:56 -0500

    Initial Commit

---
+++ Shared/SwiftUIControlsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIControlsApp.swift
+//  Shared
+//
+//  Created by gomathi saminathan on 1/21/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIControlsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
